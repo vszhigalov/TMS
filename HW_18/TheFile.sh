3243f6dc7a86cde27c579c9841353ffa8ce9479a
vszhigalov <vasiazhigalov@icloud.com>
2021-06-01 18:56:09 +0000

    The script add variable

---
+++ HW_18/TheFile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#if [ -v $(cat /etc/environment | grep TheFile)];
+if [ -v $TheFile ];
+then
+	#export File=$TheFile
+	export File=/tmp/data.file
+else
+	#export File=/tmp/data.file
+	export File=$TheFile
+fi
+echo $File
+echo $TheFile
+exit
